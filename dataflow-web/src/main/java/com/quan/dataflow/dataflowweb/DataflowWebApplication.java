d1b7bef61374c88aef937efc89536c2fb65342b9
Quan <hilda_quan@163.com>
2022-04-21 16:54:13 +0800

    22-04-21

--- dataflow-web/src/main/java/com/quan/dataflow/dataflowweb/DataflowWebApplication.java
+++ dataflow-web/src/main/java/com/quan/dataflow/dataflowweb/DataflowWebApplication.java
@@ -1,13 +1,24 @@
 package com.quan.dataflow.dataflowweb;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class DataflowWebApplication {
 
+    private final Logger logger = LoggerFactory.getLogger(DataflowWebApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(DataflowWebApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public void test() {
+        logger.info("test info");
+        logger.error("test error");
     }
 
 }
